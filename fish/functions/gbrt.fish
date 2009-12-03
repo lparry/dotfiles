728a41731683cd6fe3b23667b72bc4fc17501326
Lucas Parry <lucas@envato.com>
2009-12-03 21:22:07 +1100

    more prompt fixing, and improving readability of colors

--- fish/functions/gbrt.fish
+++ fish/functions/gbrt.fish
@@ -1,6 +1,6 @@
 function gbrt
 	for k in (git branch $argv|perl -pe 's/^..//')
-		echo -e (git show --pretty=format:"%ci (%cr) %Cblue%h%Creset %Cgreen%an%Creset" $k -- | head -n 1)\\t$k
+		gecho -e (git show --pretty=format:"%ci (%cr) %Cblue%h%Creset %Cgreen%an%Creset" $k -- | head -n 1)\\t$k
 	end | 	sort -r
 
 
