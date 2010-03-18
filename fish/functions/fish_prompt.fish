47bd2276285ecdfb3a66b12e48dfd8e852f73c85
Lucas Parry <lucas@envato.com>
2010-03-18 16:42:23 +1100

    refactor

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,15 +4,25 @@ function fish_prompt --description 'Write out the prompt'
 
   set -l time (date '+%T')
   set -l duration $CMD_DURATION
-  set -l pr_duration (test -n "$duration"; and echo "-> $duration"; or echo "")
 #  set -l pr_term (test $TERM = 'screen'; and echo "[screen/$WINDOW] ")
   set -l pr_cwd (prompt_pwd | sed 's/^\/V\/D\/U\/lparry/\~/;s/^\/V\/D\/U\/l/\~/')
   set -l pr_git_info (git_parse_branch)
   set -l status_smilie (test $previous_status -eq 0; and echo ":)"; or echo ":(")
   set -l status_color (test $previous_status -eq 0; and echo "green"; or echo "red")
 
-  test -n "$pr_git_info";and true; or set -l pr_git_info ''
+  printf '\n'
 
-  printf '%s\n%s%s %s%s %s[%s] %s%s\n%s⚡ %s' $pr_duration (set_color $status_color) $status_smilie (set_color blue) $pr_cwd (set_color yellow) $pr_git_info (set_color normal) $time (set_color $status_color) (set_color normal)
+  if test -n "$duration"
+    printf '\->%s\n' $duration
+  end
+  printf '%s%s '  (set_color $status_color) $status_smilie 
 
-end
+  printf '%s%s ' (set_color cyan) $pr_cwd 
+
+  if test -n "$pr_git_info"
+    printf '%s[%s%s%s] ' (set_color blue) (set_color yellow) $pr_git_info (set_color blue)
+  end
+  printf '%s%s ' (set_color normal) $time
+  printf '\n%s⚡ %s' (set_color $status_color) (set_color normal)
+
+  end
