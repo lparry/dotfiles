898f4c17c9292637f05bf9452adf76db15128129
Lucas Parry <lucas@envato.com>
2010-04-21 15:39:43 +1000

    only push if the pull was successful

--- fish/functions/gpo.fish
+++ fish/functions/gpo.fish
@@ -1,6 +1,5 @@
 function gpo
-  command git pull
-  if test (pwd) = '~/dev/marketplace'
+  command git pull; and if test (pwd) = '~/dev/marketplace'
     command git push origin $argv
     if test $status -eq 0
       marketplace-ci update
