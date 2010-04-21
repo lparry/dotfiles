777dba456dd4f975133ad0588e5a5df48d76b5f7
Lucas Parry <lucas@envato.com>
2010-04-21 15:24:04 +1000

    gpo now does a pull before pushing

--- fish/functions/gpo.fish
+++ fish/functions/gpo.fish
@@ -1,3 +1,11 @@
 function gpo
-  command git push origin $argv
+  command git pull
+  if test (pwd) = '~/dev/marketplace'
+    command git push origin $argv
+    if test $status -eq 0
+      marketplace-ci update
+    end
+  else
+    command git push origin $argv
+  end
 end
