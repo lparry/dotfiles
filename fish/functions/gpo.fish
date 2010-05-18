c3c87d12c5bcae11c6299695c08b879c12d07649
Lucas Parry <lucas@envato.com>
2010-05-18 13:47:52 +1000

    compacting some git stuff

--- fish/functions/gpo.fish
+++ fish/functions/gpo.fish
@@ -1,9 +1,6 @@
 function gpo
-  command git pull; and if test (pwd) = '~/dev/marketplace'
-    command git push origin $argv
-    if test $status -eq 0
-      # marketplace-ci update
-    end
+  if test (pwd) = '~/dev/marketplace'
+    command git push origin $argv #; and marketplace-ci update
   else
     command git push origin $argv
   end
