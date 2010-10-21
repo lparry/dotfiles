c26cfc6f408fe0ca6da2d420d7a0173ef551375d
Lucas Parry <lucas@envato.com>
2010-10-22 08:49:44 +1100

    piss off textmate alias to make me learn to use ack unicorn_rails config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,15 +6,16 @@ alias lal='ls -lah'
 
 alias s='git status $argv; return 0'
 alias d='gd $argv'
-alias m='mate . $argv'
+alias m='mvim $argv'
 
 alias df='df -kh $argv'
 alias du='du -k -d 1 $argv'
 alias ho='git push heroku master $argv'
-alias mr='mate Rakefile app config db features lib public script spec vendor/plugins $argv'
 alias reload_webkit='osascript -e 'tell application "WebKit" to do JavaScript "window.location.reload()" in front document''
 alias restart_rails_app='touch tmp/restart.txt; reload_webkit'
 alias tigs='tig status $argv'
 
 
-alias ss='unicorn_rails'
+alias ss='unicorn_rails --config-file ~/.unicorn.rb'
+
+alias grep='echo "use ack"'
