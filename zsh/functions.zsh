3cdfc66e262230ac65d1258b07d28fca2c15cd49
Lucas Parry <lucas@envato.com>
2011-02-22 11:42:24 +1100

    I'm sick of typing g every time i enter a directory

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,18 @@
-cd() {
-  builtin cd "$@" && ls
+function git_status_if_git_dir() {
+  git show 2> /dev/null 1> /dev/null
+  if [ $? -eq 0 ]; then
+    echo ""
+    git status
+    echo ""
+  fi
+}
+
+function cd() {
+  builtin cd "$@" && ls && git_status_if_git_dir
+}
+
+. ~/dotfiles/tools/z-zsh/z.sh
+function precmd () {
+  z --add "$(pwd -P)"
 }
 
