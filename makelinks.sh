b59e0c4d17f014fed629b26ec7b41a8f81e20321
Lucas Parry <lucas@envato.com>
2011-03-30 15:07:53 +1100

    shifted into dropbox, making resymlinking easy

---
+++ makelinks.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+rm ~/.ackrc
+rm ~/.gitconfig
+rm ~/.gitignore
+rm ~/.inputrc
+rm ~/.irbrc
+rm ~/.screenrc
+rm ~/.vim
+rm ~/.vimrc
+rm ~/.zsh
+rm ~/.zshrc
+ln -s ~/Dropbox/dotfiles/ackrc     ~/.ackrc
+ln -s ~/Dropbox/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Dropbox/dotfiles/gitignore ~/.gitignore
+ln -s ~/Dropbox/dotfiles/inputrc   ~/.inputrc
+ln -s ~/Dropbox/dotfiles/irbrc     ~/.irbrc
+ln -s ~/Dropbox/dotfiles/screenrc  ~/.screenrc
+ln -s ~/Dropbox/dotfiles/vim       ~/.vim
+ln -s ~/Dropbox/dotfiles/vimrc     ~/.vimrc
+ln -s ~/Dropbox/dotfiles/zsh       ~/.zsh
+ln -s ~/Dropbox/dotfiles/zshrc     ~/.zshrc
