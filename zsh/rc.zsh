9dbed7fd6bdaa9e55bba74b7045f44f0d57ebd67
Lucas Parry <lucas@envato.com>
2011-08-15 21:33:39 +1000

    This was shit

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -67,11 +67,6 @@ compinit -C
 ## case-insensitive (all),partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-if [[ `hostname -s` = 'lucas' ]]; then
-  ## at work
-  cd dev/envato/marketplace
-fi
-
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
 # make rails migration and edit the file
