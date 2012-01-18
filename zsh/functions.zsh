be8ee6c3b083919260f0581a2c5ff307ca5938bb
Lucas Parry <lucas@envato.com>
2012-01-19 09:35:08 +1100

    Deleted this cause I never used it

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,11 +11,6 @@ function cd() {
   builtin cd "$@" && ls && git_status_if_git_dir
 }
 
-. ~/Dropbox/dotfiles/tools/z-zsh/z.sh
-function precmd () {
-  z --add "$(pwd -P)"
-}
-
 function git-branch-owners() {
   git branch $@ |grep -v 'HEAD' | sed -e 's/  \(.*\)/echo `git show --pretty=format:"%an" "\1" |head -n1` - \1 /'|sh
 }
