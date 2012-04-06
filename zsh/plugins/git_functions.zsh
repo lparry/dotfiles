761fb4a27fb88c35f766b7128a9ee39db1925a07
Lucas Parry <lucas@envato.com>
2012-04-07 00:59:09 +1000

    Gutted the zsh git stuff to just what I wanted

--- zsh/plugins/git_functions.zsh
+++ zsh/plugins/git_functions.zsh
@@ -1,20 +1,13 @@
-# get the name of the branch we are on
-function git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo "$ZSH_THEME_GIT_PROMPT_PREFIX$(current_branch)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-}
-
-
 # Checks if working tree is dirty
-parse_git_dirty() {
+git_dirty() {
   local SUBMODULE_SYNTAX=''
   if [[ $POST_1_7_2_GIT -gt 0 ]]; then
         SUBMODULE_SYNTAX="--ignore-submodules=dirty"
   fi
   if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+    echo "1"
   else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+    echo "0"
   fi
 }
 
@@ -24,56 +17,6 @@ parse_git_dirty() {
 function current_branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo ${ref#refs/heads/}
-}
-
-# Checks if there are commits ahead from remote
-function git_prompt_ahead() {
-  if $(echo "$(git log origin/$(current_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
-    echo "$ZSH_THEME_GIT_PROMPT_AHEAD"
-  fi
-}
-
-# Formats prompt string for current git commit short SHA
-function git_prompt_short_sha() {
-  SHA=$(git rev-parse --short HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
-# Formats prompt string for current git commit long SHA
-function git_prompt_long_sha() {
-  SHA=$(git rev-parse HEAD 2> /dev/null) && echo "$ZSH_THEME_GIT_PROMPT_SHA_BEFORE$SHA$ZSH_THEME_GIT_PROMPT_SHA_AFTER"
-}
-
-# Get the status of the working tree
-git_prompt_status() {
-  INDEX=$(git status --porcelain 2> /dev/null)
-  STATUS=""
-  if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNTRACKED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^A  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  elif $(echo "$INDEX" | grep '^M  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_ADDED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^ M ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^AM ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  elif $(echo "$INDEX" | grep '^ T ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^R  ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_RENAMED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^ D ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  elif $(echo "$INDEX" | grep '^AD ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_DELETED$STATUS"
-  fi
-  if $(echo "$INDEX" | grep '^UU ' &> /dev/null); then
-    STATUS="$ZSH_THEME_GIT_PROMPT_UNMERGED$STATUS"
-  fi
-  echo $STATUS
 }
 
 #compare the provided version of git to the version installed and on path
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,21 +1,29 @@
 autoload colors; colors;
 grey='\e[0;90m'
 
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$grey%}@%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$grey%}] %{$fg[red]%}✗%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$grey%}]%{$reset_color%}"
+#replaces /Users/NAME with ~, compacts dirnames down to 1 letter for parent directories
+function short_pwd {
+  local trunc_pwd=$(pwd|sed -e "s:^/Users/$(whoami):~:" -e "s:^/home/$(whoami):~:" -e 's:\(\.\{0,1\}[^/]\)[^/]*/:\1/:g')
+  echo "%{$fg[green]%}$trunc_pwd%{$reset_color%}"
+}
+
+function rbenv_part {
+  echo "%{$fg[blue]%}$(rbenv_prompt_info)%{$reset_color%}"
+}
+
+function branch_part {
+  echo "%{$grey%}#%{$fg[yellow]%}$(current_branch)%{$reset_color%}"
+}
+
+function git_dirty_status {
+  if [[ "$(git_dirty)" == "1" ]]; then
+    echo "%{$fg[red]%}✗%{$reset_color%}"
+  fi
+}
+
 
 function lucas_prompt {
-
-  #replaces /Users/NAME with ~, compacts dirnames down to 1 letter for parent directories
-  short_pwd=$(pwd|sed -e "s:^/Users/$(whoami):~:" -e "s:^/home/$(whoami):~:" -e 's:\(\.\{0,1\}[^/]\)[^/]*/:\1/:g')
-  branch=$(current_branch)
-  rbenv_part="%{$grey%}[%{$fg[blue]%}$(rbenv_prompt_info)%{$reset_color%}"
-
-  prompt="%{$fg[green]%}$short_pwd %{$fg[red]%}%{$reset_color%} $rbenv_part$(git_prompt_info)"
-
-  echo -n $prompt
+  echo -n "$(short_pwd) %{$grey%}[%{$reset_color%}$(rbenv_part)$(branch_part)%{$grey%}]%{$reset_color%}$(git_dirty_status)"
 }
 
 setopt prompt_subst
