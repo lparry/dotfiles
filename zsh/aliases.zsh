57aa0200424e6b26d49ad0ff08f5f6ce38a6329b
Lucas Parry <lucas@envato.com>
2012-04-06 23:59:48 +1000

    Clean up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,45 +2,27 @@ alias l='ls'
 alias ls='ls -GF --color=tty'
 alias ll='ls -lh'
 alias la='ls -a'
-alias lal='ls -lah'
 
 alias tree='tree -C'
 
-alias s='git status $argv; return 0'
-alias d='gd $argv'
 alias m='cd ~/dev/envato/marketplace'
 
 alias :q='exit'
 
 alias df='df -kh $argv'
-if [[ '$(uname)' == 'Linux' ]]; then
-  alias du='du -k --max-depth=1 $argv'
-else
-  alias du='du -k -d 1 $argv'
-fi
-alias ho='git push heroku master $argv'
-alias reload_webkit='osascript -e 'tell application "WebKit" to do JavaScript "window.location.reload()" in front document''
-alias restart_rails_app='touch tmp/restart.txt; reload_webkit'
-alias tigs='tig status $argv'
+
+alias du='du -k --max-depth=1 $argv'
 
 alias ss='bin/unicorn_rails --config-file ~/.unicorn.rb'
+alias cucumber='cucumber -r features'
 
 
 if (which gsed > /dev/null) && [[ $? == 0 ]]; then
   alias sed=gsed
 fi
 
-#alias grep='echo "use ack"'
-
-alias cucumber='cucumber -r features'
-
 alias hg='nocorrect hg'
 
-#alias spec='bundle_when_gemfile_exists spec'
-#alias rake='bundle_when_gemfile_exists rake'
-#alias cap='bundle_when_gemfile_exists cap'
 alias cap_shell='cap shell ROLES=web_app'
-alias b='bundle exec'
-alias c='bundle exec cucumber'
-alias s='bundle exec spec'
+
 alias git=hub
