c3c9a864919478d125b3fea39d0d146116c4f7a9
Lucas Parry <lucas@envato.com>
2012-04-06 23:50:04 -0700

    clone vimfiles, pre-checks

--- makelinks.sh
+++ makelinks.sh
@@ -18,7 +18,15 @@ ln -s ~/Dropbox/dotfiles/screenrc  ~/.screenrc
 ln -s ~/Dropbox/dotfiles/zsh       ~/.zsh
 ln -s ~/Dropbox/dotfiles/zshrc     ~/.zshrc
 
-ln -s ~/Dropbox/dotfiles/bin       ~/bin
+if [ ! -d ~/bin ]; then
+  ln -s ~/Dropbox/dotfiles/bin       ~/bin
+fi
 
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/Dropbox/dotfiles/git-repos/zsh-syntax-highlighting
+if [ ! -d ~/Dropbox/dotfiles/git-repos/zsh-syntax-highlighting ]; then
+  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/Dropbox/dotfiles/git-repos/zsh-syntax-highlighting
+fi
+
+if [ ! -d ~/Dropbox/vimfiles ]; then
+  git clone http://lparry@github.com/lparry/vimfiles.git ~/Dropbox/vimfiles
+fi
 
