66a44c29ad4369191fc9facfad7fb826df049e4d
Lucas Parry <lucas@envato.com>
2012-04-07 12:16:14 +1000

    Update auto-linker

--- makelinks.sh
+++ makelinks.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 rm ~/.ackrc
+rm ~/.gemrc
 rm ~/.gitconfig
 rm ~/.gitignore
 rm ~/.inputrc
 rm ~/.irbrc
 rm ~/.screenrc
-rm ~/.vim
-rm ~/.vimrc
 rm ~/.zsh
 rm ~/.zshrc
 ln -s ~/Dropbox/dotfiles/ackrc     ~/.ackrc
+ln -s ~/Dropbox/dotfiles/gemrc     ~/.gemrc
 ln -s ~/Dropbox/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Dropbox/dotfiles/gitignore ~/.gitignore
 ln -s ~/Dropbox/dotfiles/inputrc   ~/.inputrc
 ln -s ~/Dropbox/dotfiles/irbrc     ~/.irbrc
 ln -s ~/Dropbox/dotfiles/screenrc  ~/.screenrc
-ln -s ~/Dropbox/dotfiles/vim       ~/.vim
-ln -s ~/Dropbox/dotfiles/vimrc     ~/.vimrc
 ln -s ~/Dropbox/dotfiles/zsh       ~/.zsh
 ln -s ~/Dropbox/dotfiles/zshrc     ~/.zshrc
-ln -s ~/Dropbox/oh-my-zsh          ~/.oh-my-zsh
+
+ln -s ~/Dropbox/dotfiles/bin       ~/bin
