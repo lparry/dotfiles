0415a884ee833386bc78f2b564f97a99590fd4a4
Lucas Parry <lucas@envato.com>
2012-04-13 09:43:41 +1000

    better prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,7 +3,7 @@ grey='\e[0;90m'
 
 #replaces /Users/NAME with ~, compacts dirnames down to 1 letter for parent directories
 function short_pwd {
-  local trunc_pwd=$(pwd|sed -e "s:^/Users/$(whoami):~:" -e "s:^/home/$(whoami):~:" -e 's:\(\.\{0,1\}[^/]\)[^/]*/:\1/:g')
+  trunc_pwd=$(pwd|sed -e "s:^/Users/$(whoami):~:" -e "s:^/home/$(whoami):~:" -e 's:\(\.\{0,1\}[^/]\)[^/]*/:\1/:g')
   echo "%{$fg[green]%}$trunc_pwd%{$reset_color%}"
 }
 
@@ -17,6 +17,10 @@ function branch_part {
   fi
 }
 
+function host_part {
+  echo "%{$grey%}@%{$fg[magenta]%}$(hostname -s)%{$reset_color%}"
+}
+
 function git_dirty_status {
   if [[ "$(git_dirty)" == "1" ]]; then
     echo "%{$fg[red]%}✗%{$reset_color%}"
@@ -25,7 +29,7 @@ function git_dirty_status {
 
 
 function lucas_prompt {
-  echo -n "$(short_pwd) %{$grey%}[%{$reset_color%}$(rbenv_part)$(branch_part)%{$grey%}]%{$reset_color%}$(git_dirty_status)"
+  echo -n "$(short_pwd) %{$grey%}[%{$reset_color%}$(rbenv_part)$(branch_part)$(host_part)%{$grey%}]%{$reset_color%}$(git_dirty_status)"
 }
 
 setopt prompt_subst
