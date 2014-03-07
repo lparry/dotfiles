7b8cdfb9e7ab83dea9e5314aad089223e791557a
Lucas Parry <lparry@gmail.com>
2014-03-06 20:21:52 -0600

    Auto click some shit

--- dotjs/hostelworld.com.js
+++ dotjs/hostelworld.com.js
@@ -2,4 +2,16 @@ stop_fucking_my_middleclicks = function() {
   $('a[class=gotoMicrosite]').removeClass('gotoMicrosite');
   setTimeout(stop_fucking_my_middleclicks, 2000)
 };
+
+dorms_only = function() {
+  $("#hide1").click();
+  $("#mixed").click();
+  $("#male").click();
+};
+
 stop_fucking_my_middleclicks();
+
+
+$(document).ready ( function(){
+  setTimeout(dorms_only, 2000)
+});
