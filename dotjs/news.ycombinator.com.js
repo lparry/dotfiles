986b2262de1fd275feaeec0415974f208d052099
Lucas Parry <lparry@gmail.com>
2014-03-06 20:25:27 -0600

    Productivity 1UP

--- dotjs/news.ycombinator.com.js
+++ dotjs/news.ycombinator.com.js
@@ -1,7 +1,3 @@
-//$("img[src='http://ycombinator.com/images/grayarrow.gif']")
-//  .parent()
-//  .html("<span style='color: #828282'>▲</span>")
-//   .attr("src","http://upload.wikimedia.org/wikipedia/commons/6/61/Black_Up_Arrow.png")
-//   .attr("height", "8px")
-
-
+$(".votearrow").hide();
+$(".subtext").hide();
+$(".subtext").siblings().hide()
